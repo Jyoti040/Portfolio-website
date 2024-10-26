ce2b78621e6d6e9c13e2a9491579866487f61e6f
Jyoti Ahuja <jyotiahuja822@gmail.com>
2024-10-27 00:51:54 +0530

    minute changes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
