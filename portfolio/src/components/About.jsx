2bb828c1c2f7e573f3b45070cb0102a71741d81c
Jyoti Ahuja <jyotiahuja822@gmail.com>
2025-04-02 18:38:46 +0530

    summary updated

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -44,12 +44,13 @@ const Typewriter = ({text,speed})=>{
   return (
     <div className='mt-10 flex justify-center items-center' id='about'>
         <div className=''>
-           <h2 className='text-3xl lg:text-5xl text-lightBlue font-semibold flex justify-center'> Hey , I'm Jyoti</h2>
-           <h3 className='pt-4 text-lg flex justify-center'>
+           <h2 className='text-3xl lg:text-5xl text-lightBlue font-semibold flex justify-center'> <Typewriter text={"Hey , I'm Jyoti"} speed={200}/></h2>
+           {/* <h3 className='pt-4 text-lg flex justify-center '>
               <Typewriter text={"I'm full stack developer"} speed={200}/>
-           </h3>
-           <h2 className='pt-4 px-2 text-center'>I'm Jyoti , a Btech student doing specialisation in computer science and engneering.
-            I am a MERN stack <br/> web developer .  I am also an open source enthusiast.
+           </h3> */}
+           <h2 className='pt-4 px-2 text-center flex justify-center'>I'm a Btech student doing specialisation in computer science and engneering.
+           I am an aspiring Software Developer with a strong foundation in full-stack development and data structures and algorithms and a passion for building dynamic and responsive web applications.
+           Driven by a passion for continuous learning and innovation, aiming to create efficient, scalable, and impactful solutions solving the real world problems , I aspire to contribute to organizations through my technical expertise, problem-solving abilities, and a proactive approach in a dynamic and growth-driven environment.
            </h2>
            <div className='flex justify-center mt-6'>
              <motion.button whileTap={{ scale: 0.85 }} whileHover={{scale : 1.10}} className='border border-lightBlue py-3 px-4 border-md text-md lg:text-lg' >
